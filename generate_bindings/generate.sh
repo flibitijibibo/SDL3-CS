09d0d91577988c8348b1b61581040e54057acfe2
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-08-08 00:10:40 -0700

    wip move generate directory; abs paths for gen script

--- generate_bindings/generate.sh
+++ generate_bindings/generate.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 || ! -d $1 || ! -d $2 ]]; then
-    echo 'usage: `generate.sh <executables subdir> <path to sdl repo root>`'
+if [[ $# -ne 1 || ! -d $1 ]]; then
+    echo 'usage: `generate.sh <path to sdl repo root>`'
     exit 0
 fi
 
-EXES_DIR=$1
-C2FFI="${EXES_DIR}/c2ffi"
-C2CS="${EXES_DIR}/c2cs"
+SDL_DIR=$1
 
-if [[ ! -e $C2FFI ]]; then
-    C2FFI="${C2FFI}.exe"
+pushd "$(dirname $0)/.." 1> /dev/null
+ROOT_DIR=`pwd`
+popd 1> /dev/null
 
-    if [[ ! -e $C2FFI ]]; then
-        echo 'c2ffi executable missing!'
-        exit 1
-    fi
+SCRIPT_DIR="${ROOT_DIR}/generate_bindings"
+
+SYSTEM_OS=`uname`
+if [[ $SYSTEM_OS = "Linux" ]]; then
+    C2FFI="${SCRIPT_DIR}/executables/linux-x64/c2ffi"
+    C2CS="${SCRIPT_DIR}/executables/linux-x64/c2cs"
+elif [[ $SYSTEM_OS = "Darwin" ]]; then
+    C2FFI="${SCRIPT_DIR}/executables/osx-arm64/c2ffi"
+    C2CS="${SCRIPT_DIR}/executables/osx-arm64/c2cs"
+else # assume it's cygwin or something
+    C2FFI="${SCRIPT_DIR}/executables/win-x64/c2ffi.exe"
+    C2CS="${SCRIPT_DIR}/executables/win-x64/c2cs.exe"
 fi
 
-if [[ ! -e $C2CS ]]; then
-    C2CS="${C2CS}.exe"
-
-    if [[ ! -e $C2CS ]]; then
-        echo 'c2cs executable missing!'
-        exit 1
-    fi
-fi
-
-
-SDL_DIR=$2
-SCR_DIR="$(dirname $0)"
-ROOT_DIR="${SCR_DIR}/.."
-GENERATE_DIR="${ROOT_DIR}/generated"
+GENERATE_DIR="${ROOT_DIR}/src/generated"
 
 TMP_DIR="$(mktemp -d)"
 echo "building in temporary directory ${TMP_DIR}..."
@@ -39,16 +33,16 @@ echo "building in temporary directory ${TMP_DIR}..."
 C2FFI_CONFIG="${TMP_DIR}/config_extract.json"
 C2FFI_OUTPUT_DIR="${TMP_DIR}/output"
 C2FFI_OUTPUT_FILEPATH="${C2FFI_OUTPUT_DIR}/cross-platform-ffi.json"
-mkdir C2FFI_OUTPUT_DIR
+mkdir $C2FFI_OUTPUT_DIR
 
-sed -e "s@TEMPLATE_SDL_PATH@${SDL_DIR}@g" -e "s@TEMPLATE_OUTPUT_DIR@${C2FFI_OUTPUT_DIR}@g" "${SCR_DIR}/config_extract.json.template" > $C2FFI_CONFIG
+sed -e "s@TEMPLATE_SDL_PATH@${SDL_DIR}@g" -e "s@TEMPLATE_OUTPUT_DIR@${C2FFI_OUTPUT_DIR}@g" "${SCRIPT_DIR}/config_extract.json.template" > $C2FFI_CONFIG
 
 $C2FFI extract --config $C2FFI_CONFIG
 $C2FFI merge --inputDirectoryPath $C2FFI_OUTPUT_DIR --outputFilePath $C2FFI_OUTPUT_FILEPATH
 
 # run c2cs --config
 C2CS_CONFIG="${TMP_DIR}/config_generate_cs.json"
-sed -e "s@TEMPLATE_C2FFI_OUTPUT@${C2FFI_OUTPUT_FILEPATH}@g" -e "s@TEMPLATE_GENERATE_DIR@${GENERATE_DIR}@g" "${SCR_DIR}/config_generate_cs.json.template" > $C2CS_CONFIG
+sed -e "s@TEMPLATE_C2FFI_OUTPUT@${C2FFI_OUTPUT_FILEPATH}@g" -e "s@TEMPLATE_GENERATE_DIR@${GENERATE_DIR}@g" "${SCRIPT_DIR}/config_generate_cs.json.template" > $C2CS_CONFIG
 
 $C2CS generate --config $C2CS_CONFIG
 
